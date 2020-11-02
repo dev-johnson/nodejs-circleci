a7916b29b623291357ad1e9f492a2b5b81b39e61
Johnson T <johnson@ideas2it.com>
2020-11-02 18:48:08 +0530

    fixed test

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,10 @@
 const { TestScheduler } = require("jest");
 
 describe('Sum two numbers', () => {
-  test('a + b = c', ()=> {
+  test('pass: a + b = c', ()=> {
     const a = 5;
     const b = 5;
     const c = 10;
-    expect(a+b).toBe(9);
+    expect(a+b).toBe(c);
   })
 });
