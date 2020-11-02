f315ea558142557892a77b255379d0bf8351bcd2
Johnson T <johnson@ideas2it.com>
2020-11-02 17:06:49 +0530

    Initial commit

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+const { TestScheduler } = require("jest");
+
+describe('Sum two numbers', () => {
+  test('a + b = c', ()=> {
+    const a = 5;
+    const b = 5;
+    const c = 10;
+    expect(a+b).toBe(c);
+  })
+});
